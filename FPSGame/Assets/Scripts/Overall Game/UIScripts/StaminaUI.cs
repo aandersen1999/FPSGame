d10ae31d36f23caee55a84b1bb600120300d7b34
fruitymario <66334659+aandersen1999@users.noreply.github.com>
2022-07-03 02:03:10 -0400

    added stamina bar

---
+++ FPSGame/Assets/Scripts/Overall Game/UIScripts/StaminaUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaUI : MonoBehaviour
+{
+    private Slider meter;
+
+    private void Awake()
+    {
+        meter = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        meter.value = GameMasterBehavior.GameMaster.playerController.stamina;
+    }
+}
