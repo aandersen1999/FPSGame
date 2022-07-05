6a223f9e1cb041621c8ad4932187486b6f176501
fruitymario <66334659+aandersen1999@users.noreply.github.com>
2022-07-05 07:04:10 -0400

    Maybe should focus on AI for now

---
+++ FPSGame/Assets/Scripts/Overall Game/UIScripts/InteractText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractText : MonoBehaviour
+{
+    private Text t;
+    private bool visible;
+
+    private void Awake()
+    {
+        t = GetComponent<Text>();
+        t.color = new Color32(50, 50, 50, 0);
+    }
+
+    private void Start()
+    {
+        t.text = "Press " + EventManager.instance.PEC.interactKey.ToString() + " to use";
+    }
+
+    private void Update()
+    {
+        ToggleText(GameMasterBehavior.GameMaster.playerController.InteractableObject != null);
+    }
+
+    private void ToggleText(bool toggle)
+    {
+        if (toggle)
+        {
+            if (!visible) 
+            {
+                t.color = new Color32(50, 50, 50, 255);
+                visible = true;
+            }
+        }
+        else
+        {
+            if (visible)
+            {
+                t.color = new Color32(50, 50, 50, 0);
+                visible = false;
+            }
+        }
+    }
+}
