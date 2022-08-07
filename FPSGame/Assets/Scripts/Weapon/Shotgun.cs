019d1c950d5e27f816a2a74675a323abe6b20ada
fruitymario <66334659+aandersen1999@users.noreply.github.com>
2022-08-07 02:08:35 -0400

    Holy shit. I thought polymorphism in unity doesn't work because GetComponent doesn't work that way this changes everything

---
+++ FPSGame/Assets/Scripts/Weapon/Shotgun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    Weapon test;
+
+    private void Start()
+    {
+        test = GetComponent<Weapon>();
+        if(test == null)
+        {
+            Debug.Log("Fail");
+        }
+    }
+}
